faf815404586d1c889dc1164e1a611597c42130b
Morgan <d274798495@gmail.com>
2015-07-02 15:49:45 +0800

    Revert de07f0c..de82f09
    This rolls back to commit de07f0c12514b9ef0601339ef0f95aadf063636f.

--- HelloWord/ViewController.m
+++ HelloWord/ViewController.m
@@ -17,7 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"first show ");
-    NSLog(@"second show");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
