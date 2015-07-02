de07f0c12514b9ef0601339ef0f95aadf063636f
Morgan <d274798495@gmail.com>
2015-07-02 15:44:43 +0800

    first helloword

---
+++ HelloWord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWord
+//
+//  Created by skyclass on 15/7/2.
+//  Copyright (c) 2015年 skyclass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWord/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  HelloWord
+//
+//  Created by skyclass on 15/7/2.
+//  Copyright (c) 2015年 skyclass. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"first show ");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
